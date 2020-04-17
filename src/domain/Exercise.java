d2a12929e81762455a93ee079234ca6660859575
zourabchik <44872022+zourabchik@users.noreply.github.com>
2020-04-17 18:09:31 +0300

    Uploaded image.

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,21 @@
 package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
-    }
-}
+	public static int recursion(int m, int n) {
+		 // Базовый случай
+		if (m == 0) {
+			return n + 1;
+			}
+		// Шаг рекурсии/рекурсивное условие
+		else if (n == 0 && m > 0) {
+			return recursion(m - 1, 1);
+			} 
+		// Шаг рекурсии/рекурсивное условие
+		else {
+			return recursion(m - 1, recursion(m, n - 1));
+			}
+		}
+	public static void main(String[] args) {
+		System.out.println(recursion(2, 2)); // вызов рекурсивной функции
+		}
+	}
