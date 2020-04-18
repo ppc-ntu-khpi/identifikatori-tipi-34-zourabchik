6df7dd8695367df227683e3bc33c3b65a78c0fd1
zourabchik <44872022+zourabchik@users.noreply.github.com>
2020-04-18 14:15:46 +0300

    fix

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -8,8 +8,8 @@ import domain.Exercise;
 public class TestResult {
 	/** Проверка основной функциональности класса */
     public static void main(String[] args) {
-        byte i = 2;
-		byte j = 2;
+        int i = 2;
+		int j = 2;
 		if(i > 0 && j > 0) {
 			/** Вызов рекурсивной функции */
 			System.out.println("Ответ " + Exercise.recursion(i, j)); 
