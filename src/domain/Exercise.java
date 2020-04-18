0d0845287bc6a4674d10ef185bbe507ced06adb1
zourabchik <44872022+zourabchik@users.noreply.github.com>
2020-04-18 13:56:02 +0300

    fix

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -4,18 +4,18 @@ package domain;
 */
 public class Exercise {
 	
-	public static int recursion(int m, int n) {
+	public static int recursion(int i, int j) {
 		 /** Базовый случай */
-		if (m == 0) {
-			return n + 1;
+		if (i == 0) {
+			return j + 1;
 			}
 		/** Шаг рекурсии/рекурсивное условие */
-		else if (n == 0 && m > 0) {
-			return recursion(m - 1, 1);
+		else if (j == 0 && i > 0) {
+			return recursion(i - 1, 1);
 			} 
 		/** Шаг рекурсии/рекурсивное условие */
 		else {
-			return recursion(m - 1, recursion(m, n - 1));
+			return recursion(i - 1, recursion(i, j - 1));
 			}
 		}
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -8,11 +8,11 @@ import domain.Exercise;
 public class TestResult {
 	/** Проверка основной функциональности класса */
     public static void main(String[] args) {
-        int n = 2;
-		int m = 2;
-		if(n > 0 && m > 0) {
+        byte i = 2;
+		byte j = 2;
+		if(i > 0 && j > 0) {
 			/** Вызов рекурсивной функции */
-			System.out.println("Ответ " + Exercise.recursion(n, m)); 
+			System.out.println("Ответ " + Exercise.recursion(i, j)); 
         }
         else{
             System.out.println("Введите положительные целые числа!!!");}
